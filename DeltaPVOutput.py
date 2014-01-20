f2ddd95b42ed35dea80e45e74aa95470d0d6be7d
stik79 <matelliotstik79@gmail.com>
2014-01-20 22:46:16 +1030

    added verbose option as requested

--- DeltaPVOutput.py
+++ DeltaPVOutput.py
@@ -1,7 +1,7 @@
 #A simple script to read values from a delta inverter and post them to 
 #PVoutput.org
 
-import time, subprocess,serial
+import time, subprocess,serial,argparse,sys
 from deltaInv import DeltaInverter
 from time import localtime, strftime
 
@@ -9,7 +9,12 @@ from time import localtime, strftime
 SYSTEMID=""
 APIKEY=""
 
+parser = argparse.ArgumentParser()
+
 if __name__ == '__main__':
+
+    parser.add_argument('-v','--verbose',help='Enable verbose output to stderr',default=False,dest='verbose_mode',required=False,action='store_true')
+    parser_results = parser.parse_args()
 
     #Edit your serial connection as required!!
     connection = serial.Serial('/dev/ttyUSB0',19200,timeout=0.2);
@@ -48,6 +53,13 @@ if __name__ == '__main__':
         response = connection.read(100)
         value = inv1.getValueFromResponse(response)
         t_temp = 'v5={0}'.format(value)
+
+	#if verbose mode
+	if parser_results.verbose_mode==True: 
+		sys.stderr.write('Date: %s, Time: %s\n' %(t_date, t_time))
+		sys.stderr.write('Energy Today: %sWh, Instantaneous Power: %sW\n' %(t_energy,t_power))
+		sys.stderr.write('Volts: %s, Temp: %s oC\n' % (t_volts,t_temp))
+		sys.stderr.flush()
 
 	#Send it all off to PVOutput.org
         cmd = ['/usr/bin/curl',
